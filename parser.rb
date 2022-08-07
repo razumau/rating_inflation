9a9a6a83e99d51bfd377d53e7578e212331cd0bd
Jury Razumau <jury.razumau@zendesk.com>
2022-08-07 15:46:13 +0200

    parse only selected places

--- parser.rb
+++ parser.rb
@@ -1,13 +1,14 @@
 require 'nokogiri'
 
 class Parser
-  def initialize(release)
+  def initialize(release, places=nil)
     @release = release
+    @places = places || (1..150)
   end
 
   def parse_release
     doc = File.open("pages/#{@release.id}.html") { |f| Nokogiri::HTML(f) }
-    (1..150).map do |place|
+    @places.map do |place|
       rating = doc.xpath("//*[@id='teams_table']/tbody/tr[#{place}]/td[4]").to_s.split[1].to_i
       RatingValue.new(@release.date, place, rating)
     end
